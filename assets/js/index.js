ffd007aebac92f1c06af84a77524e488b5a595fd
Greyknight2216 <nahuelcunz1@gmail.com>
2022-09-08 14:50:41 -0300

    added CSS and API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 setItems()
 let itemName = prompt("Item Name").toLowerCase();
 let currentItem = getItem(itemName)
-document.getElementById("item-data").innerHTML=currentItem["name"] + " " + currentItem["stat"] + " " + currentItem["price"]
+document.getElementById("item").innerHTML=currentItem["name"]
+document.getElementById("stats").innerHTML=currentItem["stat"]
+document.getElementById("price").innerHTML=currentItem["price"]
 console.log(document.getElementById("item-data"), getItem("sword"))
