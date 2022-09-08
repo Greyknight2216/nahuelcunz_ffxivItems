c8d69239f0c3876374eec539d6a328c28ab3e803
Greyknight2216 <nahuelcunz1@gmail.com>
2022-09-08 18:46:32 -0300

    array added

--- assets/js/function.js
+++ assets/js/function.js
@@ -5,33 +5,38 @@ function setItems(){
         "items": [{
                 "name": "sword",
                 "stats": "Physical Damage 50",
+                "legend": "Super cool sword",
                 "price": "500G"
             },
             {
                 "name": "Shield",
                 "stats": "Damage Reduction 70",
+                "legend": "Powerful shield",
                 "price": "700G"
             },
             {
                 "name": "Chest Plate",
                 "stats": "Defense 80",
+                "legend": "Chest Plate to resist everything",
                 "price": "1000G"
             },
             {
                 "name": "Leggings",
                 "stats": "Defense 75",
+                "legend": "Leggings for legs obviously",
                 "price": "900G"
             },
             {
                 "name": "Helmet",
                 "stats": "Defense 65",
+                "legend": "Here is the helmet of the legends",
                 "price": "650G"
             }
         ]
     }
     
     for(let i = 0; i < itemJson["items"].length; i++){
-        items[itemJson["items"][i]["name"].toLowerCase()] = new Item(itemJson["items"][i]["name"], itemJson["items"][i]["stats"], itemJson["items"][i]["price"]);
+        items[itemJson["items"][i]["name"].toLowerCase()] = new Item(itemJson["items"][i]["name"], itemJson["items"][i]["legend"], itemJson["items"][i]["stats"], itemJson["items"][i]["price"]);
     }
 }
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 setItems()
-let itemName = prompt("Item Name").toLowerCase();
+// let itemName = prompt("Item Name").toLowerCase();
 let currentItem = getItem(itemName)
 document.getElementById("item").innerHTML=currentItem["name"]
+document.getElementById("legend").innerHTML=currentItem["legend"]
 document.getElementById("stats").innerHTML=currentItem["stat"]
 document.getElementById("price").innerHTML=currentItem["price"]
-console.log(document.getElementById("item-data"), getItem("sword"))
