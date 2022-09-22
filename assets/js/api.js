ff7dd65c58d4e046673bb0dee59a1d43dfbd728a
Greyknight2216 <nahuelcunz1@gmail.com>
2022-09-22 18:04:44 -0300

    g

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,23 @@
-const XIVAPI = require('@xivapi/js')
+let itemID = prompt("Insert ID")
 
-const xiv = new XIVAPI()
+let itemData = {}
+function showData (){
+    console.log(itemData['IconHD'],['Name'],['Description_en'])    
+}
+
+function drawCard(){
+    document.getElementById('description').innerHTML=itemData['Description_en'];
+    document.getElementById('item').innerHTML=itemData['Name'];
+    let itemIcon = document.getElementById('iconImg')
+    itemIcon.src='https://xivapi.com' + itemData['IconHD']
+}
+
+fetch(`https://xivapi.com/Item/${itemID}`)
+    .then(response => response.json())
+    .then(data => {
+        itemData = data
+        showData()
+        drawCard()
+    } 
+   ) 
+
