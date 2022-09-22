797de6475711eb0439d0d80647dfb6d4a101e6c1
Greyknight2216 <nahuelcunz1@gmail.com>
2022-09-22 18:47:55 -0300

    Added Universalis DOM

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,14 +2,16 @@ let itemID = prompt("Insert ID")
 
 let itemData = {}
 function showData (){
-    console.log(itemData['IconHD'],['Name'],['Description_en'])    
+    console.log(itemData['ID'],['IconHD'],['Name'],['Description_en'])    
 }
 
 function drawCard(){
     document.getElementById('description').innerHTML=itemData['Description_en'];
     document.getElementById('item').innerHTML=itemData['Name'];
     let itemIcon = document.getElementById('iconImg')
-    itemIcon.src='https://xivapi.com' + itemData['IconHD']
+    itemIcon.src='https://xivapi.com' + itemData['IconHD'];
+    let universalis = document.getElementById('market')
+    universalis.href='https://universalis.app/market/' + itemID;
 }
 
 fetch(`https://xivapi.com/Item/${itemID}`)
