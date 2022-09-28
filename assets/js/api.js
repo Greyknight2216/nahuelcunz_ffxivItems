b398fcbaedb4243bba95304c860927186a8460d6
Greyknight2216 <nahuelcunz1@gmail.com>
2022-09-28 13:49:28 -0300

    added library and a search bar

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,26 +1,43 @@
-let itemID = prompt("Insert ID")
+let itemID = ''
+
+document.getElementById('searchInput').addEventListener('keypress', function(e) {
+    // check if the element is an input element and the key is enter
+    if (e.target.nodeName === "INPUT" && e.key === 'Enter') {
+        let result = e.target.value
+        //drawCardFunction(result)
+        if (!isNaN(result)){
+            getData(result)
+        }
+  }
+});
 
 let itemData = {}
-function showData (){
-    console.log(itemData['ID'],['IconHD'],['Name'],['Description_en'])    
+
+function showData() {
+    console.log(itemData['ID'], itemData['IconHD'], itemData['Name'], itemData['BaseParamSpecial0']['Description'])
 }
 
-function drawCard(){
-    document.getElementById('description').innerHTML=itemData['Description_en'];
-    document.getElementById('item').innerHTML=itemData['Name'];
+function drawCard() {
+    document.getElementById('description').innerHTML = itemData['Description_en'];
+    document.getElementById('item').innerHTML = itemData['Name'];
     let itemIcon = document.getElementById('iconImg')
-    itemIcon.src='https://xivapi.com' + itemData['IconHD'];
+    itemIcon.src = 'https://xivapi.com' + itemData['IconHD'];
     let universalis = document.getElementById('market')
-    universalis.href='https://universalis.app/market/' + itemID;
+    universalis.href = 'https://universalis.app/market/' + itemID;
 }
 
-fetch(`https://xivapi.com/Item/${itemID}`)
+function getData(itemID) {
+
+    fetch(`https://xivapi.com/Item/${itemID}`)
     .then(response => response.json())
     .then(data => {
         itemData = data
         showData()
         drawCard()
-    } 
-   ) 
+    })
+
+}
+
+
 
 let hello = console.log('hello')
--- assets/js/toggler.js
+++
@@ -1,7 +0,0 @@
-
-let btn = document.getElementById('btnToggler')
-    btn.addEventListener("click", chocoboMode)
-    function chocoboMode(){
-    let element = document.body;
-    element.classList.toggle("chocoboMode")
-}
